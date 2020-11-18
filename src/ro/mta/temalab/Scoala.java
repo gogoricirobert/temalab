97bc66f168a7e76edee85e4e18e903f6da715229
andreigavrila98 <andreicatalingavrila@gmail.com>
2020-11-18 13:32:45 +0200

    Adaugare metoda addProfesori
    Signed-off-by: andreigavrila98 <andreicatalingavrila@gmail.com>

--- src/ro/mta/temalab/Scoala.java
+++ src/ro/mta/temalab/Scoala.java
@@ -4,16 +4,18 @@ import java.util.ArrayList;
 
 public class Scoala {
     private ArrayList<Elev> Elevi;
+    private ArrayList<Profesori> Profesori;
     private String Name;
     public Scoala(String Name)
     {
         this.Name=Name;
         Elevi=new ArrayList<>();
+        Profesori=new ArrayList<>();
     }
     public void addElev(Elev elev)
     {
         Elevi.add((elev)) ;
     }
     public void removeElev(Elev elev){Elevi.remove(elev);}
-
+    public void addProfesori(Profesori profesori){Profesori.add(profesori);}
 }
